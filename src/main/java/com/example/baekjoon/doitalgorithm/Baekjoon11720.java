0da1cbbaf759cebab673499106373cfcdeacde9a
하늘 <gureumi74@naver.com>
2023-04-23 01:28:26 +0900

    fix: Baekjoon11720 코드 공백 수정

--- src/main/java/com/example/baekjoon/doitalgorithm/Baekjoon11720.java
+++ src/main/java/com/example/baekjoon/doitalgorithm/Baekjoon11720.java
@@ -1,12 +1,11 @@
 package com.example.baekjoon.doitalgorithm;
 
-import java.io.*;
 import java.util.Scanner;
 
 
 public class Baekjoon11720 {
     // 숫자의 합
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         char[] numbers = sc.next().toCharArray();
@@ -17,7 +16,5 @@ public class Baekjoon11720 {
         }
 
         System.out.println(sum);
-
     }
-
 }
