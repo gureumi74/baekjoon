b61bd8945965ee6f037742cf3141e4f1c2c27ad4
하늘 <gureumi74@naver.com>
2023-04-25 01:17:03 +0900

    feat: Baekjoon 11382 풀이

---
+++ src/main/java/com/example/baekjoon/basic/Beakjoon11382.java
@@ -0,0 +1,10 @@
+package com.example.baekjoon.basic;
+
+import java.util.Scanner;
+
+public class Beakjoon11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+    }
+}
