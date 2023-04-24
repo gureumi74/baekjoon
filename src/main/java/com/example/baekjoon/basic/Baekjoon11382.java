a61f69aec54c89ad6fff71a4334c28d1745d7bcb
하늘 <gureumi74@naver.com>
2023-04-25 01:29:40 +0900

    feat: Baekjoon 25314 풀이

--- src/main/java/com/example/baekjoon/basic/Baekjoon11382.java
+++ src/main/java/com/example/baekjoon/basic/Baekjoon11382.java
@@ -2,7 +2,7 @@ package com.example.baekjoon.basic;
 
 import java.util.Scanner;
 
-public class Beakjoon11382 {
+public class Baekjoon11382 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
